77759ff386072f16dc78891c7d648b9cafa9341f
CassandraThalia <W0449188@nscc.ca>
2020-11-22 18:24:51 -0400

    working EVEN BETTER now

--- sockcalc.js
+++ sockcalc.js
@@ -1,17 +1,17 @@
 // Pre-set pattern functions
 // function rightTwistSt()
 // {
-//     return "Row One</b>:&nbsp;*rt, k2* repeat to end<br><b>Row Two:</b>&nbsp;k to end<br><b>Row Three:</b>&nbsp;k to end<br><b>Row Four:</b>&nbsp;k to end";
+//     return "<b>Row One</b></b>:&nbsp;*rt, k2* repeat to end<br><b>Row Two:</b>&nbsp;k to end<br><b>Row Three:</b>&nbsp;k to end<br><b>Row Four:</b>&nbsp;k to end";
 // }
 
 // function seedSt()
 // {
-//     return "<b>Row One:</b>&nbsp;*k1, p1* repeat to end<br><b>Row Two:</b>&nbsp;*p1, k1* repeat to end";
+//     return "<b><b>Row One</b>:</b>&nbsp;*k1, p1* repeat to end<br><b>Row Two:</b>&nbsp;*p1, k1* repeat to end";
 // }
 
 // function waffelSt()
 // {
-//     return "<b>Row One:</b>&nbsp;*k2, p2* repeat to end<br><b>Row Two:</b>&nbsp;k to end";
+//     return "<b><b>Row One</b>:</b>&nbsp;*k2, p2* repeat to end<br><b>Row Two:</b>&nbsp;k to end";
 // }
 
 function doubleRibSt()
@@ -32,7 +32,7 @@ function singleRibSt()
 function rowMessage(rowNum, rowPat, numStInPtn) 
 {
     var numOfStitches = document.getElementById("numOfStsSelect").value;
-    return "Row " + rowNum + ":</b>&nbsp;*" + rowPat + "*, repeat from * to * " + ((numOfStitches / numStInPtn) - numStInPtn) + " times";
+    return "<b>Row " + rowNum + ":</b>&nbsp;*" + rowPat + "*, repeat from * to * " + ((numOfStitches / numStInPtn) - numStInPtn) + " times";
 }
 
 function repeatMessage(untilCondition)
@@ -78,19 +78,19 @@ function buildLegPattern()
 function buildHeelPattern()
 {
     var numOfStitches = document.getElementById("numOfStsSelect").value;
-    document.getElementById("heelStCalc").innerHTML = "Set-up row: k" + (numOfStitches / 2) + " stitches, turn</p>\
-        <p>Row One: slip 1 st wyif, k1, *sl1, k1*, repeat from * to *, turn</p>\
-        <p>Row Two: slip 1 st wyif, p to end of row, turn</p>";
+    document.getElementById("heelStCalc").innerHTML = "<p>Set-up row: k" + (numOfStitches / 2) + " stitches, turn</p>\
+        <p><b>Row One</b>: slip 1 st wyif, k1, *sl1, k1*, repeat from * to *, turn</p>\
+        <p><b>Row Two</b>: slip 1 st wyif, p to end of row, turn</p>";
     document.getElementById("heelRowCalc").innerHTML = "Repeat rows 1 & 2 until heel measures " + (numOfStitches / 2) + " rows total";
 }
 
 function buildheelTurnPattern()
 {
     var numOfStitches = document.getElementById("numOfStsSelect").value;
-    document.getElementById("heelTurn").innerHTML = "<p>Row One: slip 1 st wyif, k" + ((numOfStitches / 4) + 2) + ", ssk, k1, turn</p>\
-        <p>Row Two: slip 1 st wyif, p7, p2tog, p1, turn</p>\
-        <p>Row Three: slip 1 st wyif, k8, ssk, k1, turn</p>\
-        <p>Row Four: slip 1 st wyif, p9, p2tog, p1, turn</p>\
+    document.getElementById("heelTurn").innerHTML = "<p><b>Row One</b>: slip 1 st wyif, k" + ((numOfStitches / 4) + 2) + ", ssk, k1, turn</p>\
+        <p><b>Row Two</b>: slip 1 st wyif, p7, p2tog, p1, turn</p>\
+        <p><b>Row Three</b>: slip 1 st wyif, k8, ssk, k1, turn</p>\
+        <p><b>Row Four</b>: slip 1 st wyif, p9, p2tog, p1, turn</p>\
         <p>Continue in pattern until all heel sts are on your needles</p>";
 }
 
@@ -99,37 +99,37 @@ function instepPattern()
     var numOfStitches = document.getElementById("numOfStsSelect").value;
     var numOfHeelSts = numOfStitches / 2
     var stsBtwEndOfPtnAndMarker = (numOfHeelSts + ((numOfHeelSts) - (numOfStitches / 4) + 4))
-    document.getElementById("instepOutput").innerHTML = "<p>Row One: pick up " + numOfHeelSts / 2 + " stitches from side of heel flap, k" + numOfHeelSts + " IN PATTERN, pick up " + numOfHeelSts / 2 + " from other side of heel flap, k to end of heel turn</p>\
-        <p>Row Two (set-up row): k" + ((numOfHeelSts / 2) - 2) + ", ssk, k" + numOfHeelSts + " IN PATTERN, k2tog, k to end of heel turn</p>\
-        <p>Row Three: k" + ((numOfHeelSts / 2) - 3) +", k2tog, k" + numOfHeelSts + " IN PATTERN, ssk, k to end of heel turn</p>\
-        <p>Row Four: k all sts</p>\
-        <p>Row Five: k" + ((numOfHeelSts / 2) - 4) +", k2tog, k" + numOfHeelSts + " IN PATTERN, ssk, k to end of heel turn</p>\
-        <p>Row Six: k all sts</p>\
+    document.getElementById("instepOutput").innerHTML = "<p><b>Row One</b>: pick up " + numOfHeelSts / 2 + " stitches from side of heel flap, k" + numOfHeelSts + " IN PATTERN, pick up " + numOfHeelSts / 2 + " from other side of heel flap, k to end of heel turn</p>\
+        <p><b>Row Two</b> (set-up row): k" + ((numOfHeelSts / 2) - 2) + ", ssk, k" + numOfHeelSts + " IN PATTERN, k2tog, k to end of heel turn</p>\
+        <p><b>Row Three</b>: k" + ((numOfHeelSts / 2) - 3) +", k2tog, k" + numOfHeelSts + " IN PATTERN, ssk, k to end of heel turn</p>\
+        <p><b>Row Four</b>: k all sts</p>\
+        <p><b>Row Five</b>: k" + ((numOfHeelSts / 2) - 4) +", k2tog, k" + numOfHeelSts + " IN PATTERN, ssk, k to end of heel turn</p>\
+        <p><b>Row Six</b>: k all sts</p>\
         <p>Continue in pattern until you once again have " + numOfStitches + " on your needles";
 }
 
 function toePattern()
 {
     var numOfStitches = document.getElementById("numOfStsSelect").value;
-    document.getElementById("toePatOutput").innerHTML = "Row One: ssk, k" + ((numOfStitches / 2) - 4) + ", k2tog, ssk, k" + ((numOfStitches / 2) - 4) + ", k2tog</p>\
-        <p>Row Two: k all stitches</p>\
-        <p>Row Three: k all stitches</p>\
-        <p>Row Four: ssk, k" + ((numOfStitches / 2) - 6) + ", k2tog, ssk, k" + ((numOfStitches / 2) - 6) + ", k2tog</p>\
-        <p>Row Five: k all stitches</p>\
-        <p>Row Six: k all stitches</p>\
-        <p>Row Seven: <p>Row Four: ssk, k" + ((numOfStitches / 2) - 8) + ", k2tog, ssk, k" + ((numOfStitches / 2) - 8) + ", k2tog</p>\
-        <p>Row Eight: k all stitches</p>\
-        <p>Row Nine: k all stitches</p>\
-        <p>Row Ten: <p>Row Four: ssk, k" + ((numOfStitches / 2) - 10) + ", k2tog, ssk, k" + ((numOfStitches / 2) - 10) + ", k2tog</p>\
-        <p>Row Eleven: k all stitches</p>\
-        <p>Row Twelve: ssk, k" + ((numOfStitches / 2) - 12) + ", k2tog, ssk, k" + ((numOfStitches / 2) - 12) + ", k2tog</p>\
-        <p>Row Thirteen: k all stitches</p>\
-        <p>Row Fourteen: ssk, k" + ((numOfStitches / 2) - 14) + ", k2tog, ssk, k" + ((numOfStitches / 2) - 14) + ", k2tog</p>\
-        <p>Row Fifteen: k all stitches</p>\
-        <p>Row Sixteen: ssk, k" + ((numOfStitches / 2) - 16) + ", k2tog, ssk, k" + ((numOfStitches / 2) - 16) + ", k2tog</p>\
-        <p>Row Seventeen: ssk, k" + ((numOfStitches / 2) - 18) + ", k2tog, ssk, k" + ((numOfStitches / 2) - 18) + ", k2tog</p>\
-        <p>Row Eightteen: ssk, k" + ((numOfStitches / 2) - 20) + ", k2tog, ssk, k" + ((numOfStitches / 2) - 20) + ", k2tog</p>\
-        <p>Row Nineteen: ssk, k" + ((numOfStitches / 2) - 22) + ", k2tog, ssk, k" + ((numOfStitches / 2) - 22) + ", k2tog</p>";
+    document.getElementById("toePatOutput").innerHTML = "<p><b>Row One</b>: ssk, k" + ((numOfStitches / 2) - 4) + ", k2tog, ssk, k" + ((numOfStitches / 2) - 4) + ", k2tog</p>\
+        <p><b>Row Two</b>: k all stitches</p>\
+        <p><b>Row Three</b>: k all stitches</p>\
+        <p><b>Row Four</b>: ssk, k" + ((numOfStitches / 2) - 6) + ", k2tog, ssk, k" + ((numOfStitches / 2) - 6) + ", k2tog</p>\
+        <p><b>Row Five</b>: k all stitches</p>\
+        <p><b>Row Six</b>: k all stitches</p>\
+        <p><b>Row Seven</b>: ssk, k" + ((numOfStitches / 2) - 8) + ", k2tog, ssk, k" + ((numOfStitches / 2) - 8) + ", k2tog</p>\
+        <p><b>Row Eight</b>: k all stitches</p>\
+        <p><b>Row Nine</b>: k all stitches</p>\
+        <p><b>Row Ten</b>: ssk, k" + ((numOfStitches / 2) - 10) + ", k2tog, ssk, k" + ((numOfStitches / 2) - 10) + ", k2tog</p>\
+        <p><b>Row Eleven</b>: k all stitches</p>\
+        <p><b>Row Twelve</b>: ssk, k" + ((numOfStitches / 2) - 12) + ", k2tog, ssk, k" + ((numOfStitches / 2) - 12) + ", k2tog</p>\
+        <p><b>Row Thirteen</b>: k all stitches</p>\
+        <p><b>Row Fourteen</b>: ssk, k" + ((numOfStitches / 2) - 14) + ", k2tog, ssk, k" + ((numOfStitches / 2) - 14) + ", k2tog</p>\
+        <p><b>Row Fifteen</b>: k all stitches</p>\
+        <p><b>Row Sixteen</b>: ssk, k" + ((numOfStitches / 2) - 16) + ", k2tog, ssk, k" + ((numOfStitches / 2) - 16) + ", k2tog</p>\
+        <p><b>Row Seventeen</b>: ssk, k" + ((numOfStitches / 2) - 18) + ", k2tog, ssk, k" + ((numOfStitches / 2) - 18) + ", k2tog</p>\
+        <p><b>Row Eightteen</b>: ssk, k" + ((numOfStitches / 2) - 20) + ", k2tog, ssk, k" + ((numOfStitches / 2) - 20) + ", k2tog</p>\
+        <p><b>Row Nineteen</b>: ssk, k" + ((numOfStitches / 2) - 22) + ", k2tog, ssk, k" + ((numOfStitches / 2) - 22) + ", k2tog</p>";
 }
 
 function buildSockPattern()
@@ -143,8 +143,9 @@ function buildSockPattern()
     toePattern();
 }
 
-function buildAndShowPattern() {
+function buildAndShowPattern() 
+{
     buildSockPattern();
     document.getElementById("pattern").style.display = "block";
-  }
+}
 
